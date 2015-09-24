cf8f505fd0911185d5dab120bb4f8ea3f179fe0a
Brett Timperman <brett.timperman@gmail.com>
2015-09-24 07:35:20 -0400

    go fmt

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/timperman/dispatcher/plugin"
 
 func main() {
-  plugin.Start(":8081")
+	plugin.Start(":8081")
 }
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,57 +1,57 @@
 package plugin
 
 import (
-  "log"
-  "encoding/json"
-  "net/http"
-  "github.com/timperman/dispatcher/network"
+	"encoding/json"
+	"github.com/timperman/dispatcher/network"
+	"log"
+	"net/http"
 )
 
 type Handshake struct {
-  Implements []string
+	Implements []string
 }
 
 type Capabilities struct {
-  Scope string
+	Scope string
 }
 
 func Start(addr string) {
-  http.HandleFunc("/Plugin.Activate", activate)
-  http.HandleFunc("/NetworkDriver.GetCapabilities", getCapabilities)
+	http.HandleFunc("/Plugin.Activate", activate)
+	http.HandleFunc("/NetworkDriver.GetCapabilities", getCapabilities)
 
-  n, _ := network.New()
+	n, _ := network.New()
 
-  m := map[string]map[string]func(http.ResponseWriter, *http.Request){
-    "POST": {
-      "/NetworkDriver.CreateNetwork": n.CreateNetwork,
-      "/NetworkDriver.DeleteNetwork": n.DeleteNetwork,
-      "/NetworkDriver.CreateEndpoint": n.CreateEndpoint,
-      "/NetworkDriver.DeleteEndpoint": n.DeleteEndpoint,
-      "/NetworkDriver.EndpointOperInfo": n.EndpointOperInfo,
-      "/NetworkDriver.Join": n.Join,
-      "/NetworkDriver.Leave": n.Leave,
-    },
-  }
+	m := map[string]map[string]func(http.ResponseWriter, *http.Request){
+		"POST": {
+			"/NetworkDriver.CreateNetwork":    n.CreateNetwork,
+			"/NetworkDriver.DeleteNetwork":    n.DeleteNetwork,
+			"/NetworkDriver.CreateEndpoint":   n.CreateEndpoint,
+			"/NetworkDriver.DeleteEndpoint":   n.DeleteEndpoint,
+			"/NetworkDriver.EndpointOperInfo": n.EndpointOperInfo,
+			"/NetworkDriver.Join":             n.Join,
+			"/NetworkDriver.Leave":            n.Leave,
+		},
+	}
 
-  for method, routes := range m {
-    for route, f := range routes {
-      http.HandleFunc(route, handleFuncByMethod(method, f))
-    }
-  }
+	for method, routes := range m {
+		for route, f := range routes {
+			http.HandleFunc(route, handleFuncByMethod(method, f))
+		}
+	}
 
-  log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func activate(w http.ResponseWriter, r *http.Request) {
-  log.Println("Activate call")
-  if b, err := json.Marshal(&Handshake{ Implements: []string{ "NetworkDriver" }, }); err == nil {
-    w.Write(b)
-  }
+	log.Println("Activate call")
+	if b, err := json.Marshal(&Handshake{Implements: []string{"NetworkDriver"}}); err == nil {
+		w.Write(b)
+	}
 }
 
 func getCapabilities(w http.ResponseWriter, r *http.Request) {
-  log.Println("GetCapabilities call")
-  if b, err := json.Marshal(&Capabilities{ Scope: "local", }); err == nil {
-    w.Write(b)
-  }
+	log.Println("GetCapabilities call")
+	if b, err := json.Marshal(&Capabilities{Scope: "local"}); err == nil {
+		w.Write(b)
+	}
 }
